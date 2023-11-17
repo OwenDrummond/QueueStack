8b63ec3a10729dd5a8a739f65480357ef60fc4a4
OwenDrummond <129247532+OwenDrummond@users.noreply.github.com>
2023-11-17 01:14:40 -0600

    Add files via upload

---
+++ Stack.java
@@ -0,0 +1,12 @@
+package drive;
+public abstract class Stack extends LinkedList {
+    public abstract void push(String data);
+
+    public abstract Node pop();
+
+    public abstract Node peek();
+    
+    public abstract boolean isEmpty();
+    
+    public abstract int GetLength();
+}
